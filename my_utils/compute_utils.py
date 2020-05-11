1031cd16121fedb881778a6aa691291bf8d3b39e
TAOSHss <764152567@qq.com>
2020-05-11 15:36:08 +0800

    ts

---
+++ my_utils/compute_utils.py
@@ -0,0 +1,176 @@
+import time
+import numpy as np
+import os
+import torch
+import shutil
+
+
+def test_validate(val_loader, model, criterion,args):
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    # switch to evaluate mode
+    model.eval()
+
+    end = time.time()
+    for i, (input, target) in enumerate(val_loader):
+        target = target.cuda()
+
+        output = model(input)
+        loss = criterion(output, target)
+
+        # measure accuracy and record loss
+        err1, err5 = accuracy(output.data, target, topk=(1, 5))
+
+        losses.update(loss.item(), input.size(0))
+
+        top1.update(err1.item(), input.size(0))
+        top5.update(err5.item(), input.size(0))
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_freq == 0 and args.verbose == True:
+            print('Test (on val set): [{0}/{1}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Top 1-err {top1.val:.4f} ({top1.avg:.4f})\t'
+                  'Top 5-err {top5.val:.4f} ({top5.avg:.4f})'.format(
+                i, len(val_loader), batch_time=batch_time, loss=losses,
+                top1=top1, top5=top5))
+
+    return top1.avg, top5.avg, losses.avg
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+        wrong_k = batch_size - correct_k
+        res.append(wrong_k.mul_(100.0 / batch_size))
+
+    return res
+
+def rand_bbox(size, lam):
+    W = size[2]
+    H = size[3]
+    cut_rat = np.sqrt(1. - lam)
+    cut_w = np.int(W * cut_rat)
+    cut_h = np.int(H * cut_rat)
+
+    # uniform
+    cx = np.random.randint(W)
+    cy = np.random.randint(H)
+
+    bbx1 = np.clip(cx - cut_w // 2, 0, W)
+    bby1 = np.clip(cy - cut_h // 2, 0, H)
+    bbx2 = np.clip(cx + cut_w // 2, 0, W)
+    bby2 = np.clip(cy + cut_h // 2, 0, H)
+
+    return bbx1, bby1, bbx2, bby2
+
+
+def train_validate(val_loader, model, criterion, epoch,args):
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    # switch to evaluate mode
+    model.eval()
+
+    end = time.time()
+    for i, (input, target) in enumerate(val_loader):
+        target = target.cuda()
+
+        output = model(input)
+        loss = criterion(output, target)
+
+        # measure accuracy and record loss
+        err1, err5 = accuracy(output.data, target, topk=(1, 5))
+
+        losses.update(loss.item(), input.size(0))
+
+        top1.update(err1.item(), input.size(0))
+        top5.update(err5.item(), input.size(0))
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_freq == 0 and args.verbose == True:
+            print('Test (on val set): [{0}/{1}][{2}/{3}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Top 1-err {top1.val:.4f} ({top1.avg:.4f})\t'
+                  'Top 5-err {top5.val:.4f} ({top5.avg:.4f})'.format(
+                epoch, args.epochs, i, len(val_loader), batch_time=batch_time, loss=losses,
+                top1=top1, top5=top5))
+
+    print('* Epoch: [{0}/{1}]\t Top 1-err {top1.avg:.3f}  Top 5-err {top5.avg:.3f}\t Test Loss {loss.avg:.3f}'.format(
+        epoch, args.epochs, top1=top1, top5=top5, loss=losses))
+    return top1.avg, top5.avg, losses.avg
+
+
+def save_checkpoint(state, is_best,args,filename='checkpoint.pth.tar'):
+    directory = "runs/%s/" % (args.expname)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    filename = directory + filename
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, 'runs/%s/' % (args.expname) + 'model_best.pth.tar')
+
+
+def adjust_learning_rate(optimizer, epoch,args):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    if args.dataset.startswith('cifar'):
+        lr = args.lr * (0.1 ** (epoch // (args.epochs * 0.5))) * (0.1 ** (epoch // (args.epochs * 0.75)))
+    elif args.dataset == ('imagenet'):
+        if args.epochs == 300:
+            lr = args.lr * (0.1 ** (epoch // 75))
+        else:
+            lr = args.lr * (0.1 ** (epoch // 30))
+
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def get_learning_rate(optimizer):
+    lr = []
+    for param_group in optimizer.param_groups:
+        lr += [param_group['lr']]
+    return lr
+
+
+
--- train.py
+++ train.py
@@ -1,9 +1,4 @@
 import argparse
-import os
-import shutil
-import time
-
-import torch
 import torch.nn as nn
 import torch.nn.parallel
 import torch.backends.cudnn as cudnn
@@ -13,9 +8,10 @@ import torch.utils.data.distributed
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torchvision.models as models
-import resnet as RN
-import pyramidnet as PYRM
-import utils
+from backbone import resnet as RN
+from backbone import pyramidnet as PYRM
+from my_utils.compute_utils import *
+from my_utils import aug_utils
 import numpy as np
 
 import warnings
@@ -112,9 +108,9 @@ def main():
         normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                          std=[0.229, 0.224, 0.225])
 
-        jittering = utils.ColorJitter(brightness=0.4, contrast=0.4,
+        jittering = aug_utils.ColorJitter(brightness=0.4, contrast=0.4,
                                       saturation=0.4)
-        lighting = utils.Lighting(alphastd=0.1,
+        lighting = aug_utils.Lighting(alphastd=0.1,
                                   eigval=[0.2175, 0.0188, 0.0045],
                                   eigvec=[[-0.5675, 0.7192, 0.4009],
                                           [-0.5808, -0.0045, -0.8140],
@@ -176,13 +172,13 @@ def main():
 
     for epoch in range(0, args.epochs):
 
-        adjust_learning_rate(optimizer, epoch)
+        adjust_learning_rate(optimizer, epoch,args)
 
         # train for one epoch
         train_loss = train(train_loader, model, criterion, optimizer, epoch)
 
         # evaluate on validation set
-        err1, err5, val_loss = validate(val_loader, model, criterion, epoch)
+        err1, err5, val_loss = train_validate(val_loader, model, criterion, epoch,args)
 
         # remember best prec@1 and save checkpoint
         is_best = err1 <= best_err1
@@ -272,136 +268,6 @@ def train(train_loader, model, criterion, optimizer, epoch):
         epoch, args.epochs, top1=top1, top5=top5, loss=losses))
 
     return losses.avg
-
-
-def rand_bbox(size, lam):
-    W = size[2]
-    H = size[3]
-    cut_rat = np.sqrt(1. - lam)
-    cut_w = np.int(W * cut_rat)
-    cut_h = np.int(H * cut_rat)
-
-    # uniform
-    cx = np.random.randint(W)
-    cy = np.random.randint(H)
-
-    bbx1 = np.clip(cx - cut_w // 2, 0, W)
-    bby1 = np.clip(cy - cut_h // 2, 0, H)
-    bbx2 = np.clip(cx + cut_w // 2, 0, W)
-    bby2 = np.clip(cy + cut_h // 2, 0, H)
-
-    return bbx1, bby1, bbx2, bby2
-
-
-def validate(val_loader, model, criterion, epoch):
-    batch_time = AverageMeter()
-    losses = AverageMeter()
-    top1 = AverageMeter()
-    top5 = AverageMeter()
-
-    # switch to evaluate mode
-    model.eval()
-
-    end = time.time()
-    for i, (input, target) in enumerate(val_loader):
-        target = target.cuda()
-
-        output = model(input)
-        loss = criterion(output, target)
-
-        # measure accuracy and record loss
-        err1, err5 = accuracy(output.data, target, topk=(1, 5))
-
-        losses.update(loss.item(), input.size(0))
-
-        top1.update(err1.item(), input.size(0))
-        top5.update(err5.item(), input.size(0))
-
-        # measure elapsed time
-        batch_time.update(time.time() - end)
-        end = time.time()
-
-        if i % args.print_freq == 0 and args.verbose == True:
-            print('Test (on val set): [{0}/{1}][{2}/{3}]\t'
-                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                  'Top 1-err {top1.val:.4f} ({top1.avg:.4f})\t'
-                  'Top 5-err {top5.val:.4f} ({top5.avg:.4f})'.format(
-                epoch, args.epochs, i, len(val_loader), batch_time=batch_time, loss=losses,
-                top1=top1, top5=top5))
-
-    print('* Epoch: [{0}/{1}]\t Top 1-err {top1.avg:.3f}  Top 5-err {top5.avg:.3f}\t Test Loss {loss.avg:.3f}'.format(
-        epoch, args.epochs, top1=top1, top5=top5, loss=losses))
-    return top1.avg, top5.avg, losses.avg
-
-
-def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
-    directory = "runs/%s/" % (args.expname)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    filename = directory + filename
-    torch.save(state, filename)
-    if is_best:
-        shutil.copyfile(filename, 'runs/%s/' % (args.expname) + 'model_best.pth.tar')
-
-
-class AverageMeter(object):
-    """Computes and stores the average and current value"""
-
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
-
-
-def adjust_learning_rate(optimizer, epoch):
-    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-    if args.dataset.startswith('cifar'):
-        lr = args.lr * (0.1 ** (epoch // (args.epochs * 0.5))) * (0.1 ** (epoch // (args.epochs * 0.75)))
-    elif args.dataset == ('imagenet'):
-        if args.epochs == 300:
-            lr = args.lr * (0.1 ** (epoch // 75))
-        else:
-            lr = args.lr * (0.1 ** (epoch // 30))
-
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
-
-
-def get_learning_rate(optimizer):
-    lr = []
-    for param_group in optimizer.param_groups:
-        lr += [param_group['lr']]
-    return lr
-
-
-def accuracy(output, target, topk=(1,)):
-    """Computes the precision@k for the specified values of k"""
-    maxk = max(topk)
-    batch_size = target.size(0)
-
-    _, pred = output.topk(maxk, 1, True, True)
-    pred = pred.t()
-    correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-    res = []
-    for k in topk:
-        correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
-        wrong_k = batch_size - correct_k
-        res.append(wrong_k.mul_(100.0 / batch_size))
-
-    return res
-
 
 if __name__ == '__main__':
     main()
